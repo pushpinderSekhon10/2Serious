f00ba900fb45ace076736c056fe5a5bf18c30248
nshang3 <nshang3@uwo.ca>
2024-03-22 19:04:32 -0400

    worked on script

--- Assets/Scripts/EnemyDamageDealer.cs
+++ Assets/Scripts/EnemyDamageDealer.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class EnemyDamageDealer : MonoBehaviour
 {
+    bool canDealDamage;
+    bool hasDealDamage;
+
+    [SerializeField] float weaponLength;
+    [SerializeField] float weaponDamage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        canDealDamage = false;
+        hasDealDamage = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (canDealDamage && !hasDealDamage)
+        {
+            RaycastHit hit;
+            int layerMask = 1 << 8;
+
+        }
     }
 }
